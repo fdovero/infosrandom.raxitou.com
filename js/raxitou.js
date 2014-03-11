5b5669c971bbf0371f52cd54efe8abc9025c8e6b
Fabien Dovero <fabien_at_dovero.org>
2014-03-11 15:44:13 +0100

    added animations

--- js/raxitou.js
+++ js/raxitou.js
@@ -1,6 +1,37 @@
-function raxit() {
+function raxit() { 
     $.getJSON('raxitou.json', function(data) {
-        $('#rax').html(_.first(_.shuffle(data.intro))+' '+_.first(_.shuffle(data.objet))+' '+_.first(_.shuffle(data.cntxt))+' '+_.first(_.shuffle(data.chute))+' '+_.first(_.shuffle(data.concl)));
+        
+        $('#intro').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+            $('#intro').html(_.sample(data.intro));
+            $('#intro').removeClass('fadeOutDown').addClass('fadeInDown');
+        });
+
+        $('#objet').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+            $('#objet').html(_.sample(data.objet));
+            $('#objet').removeClass('fadeOutDown').addClass('fadeInDown');
+        });
+
+        $('#cntxt').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+            $('#cntxt').html(_.sample(data.cntxt));
+            $('#cntxt').removeClass('fadeOutDown').addClass('fadeInDown');
+        });
+
+        $('#chute').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+            $('#chute').html(_.sample(data.chute));
+            $('#chute').removeClass('fadeOutDown').addClass('fadeInDown');
+        });
+
+        $('#concl').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+            $('#concl').html(_.sample(data.concl));
+            $('#concl').removeClass('fadeOutDown').addClass('fadeInDown');
+        });
+
+        
+        $('#intro').removeClass('fadeInDown').addClass('animated fadeOutDown').css('-moz-animation-delay',_.random(0,3)*100+'ms').css('-moz-animation-duration','300ms');
+        $('#objet').removeClass('fadeInDown').addClass('animated fadeOutDown').css('-moz-animation-delay',_.random(0,3)*100+'ms').css('-moz-animation-duration','300ms');
+        $('#cntxt').removeClass('fadeInDown').addClass('animated fadeOutDown').css('-moz-animation-delay',_.random(0,3)*100+'ms').css('-moz-animation-duration','300ms');
+        $('#chute').removeClass('fadeInDown').addClass('animated fadeOutDown').css('-moz-animation-delay',_.random(0,3)*100+'ms').css('-moz-animation-duration','300ms');
+        $('#concl').removeClass('fadeInDown').addClass('animated fadeOutDown').css('-moz-animation-delay',_.random(0,3)*100+'ms').css('-moz-animation-duration','300ms');
     }); 
     return false;
 };
